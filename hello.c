cd2c75da2c62f254f7e35b7d7a336fd9160437e3
Willian Silva <willian.silva@concretesolutions.com.br>
2016-01-27 14:56:41 -0200

    tudoooo
    tudooooo com slack

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main()
 		printf("Que merda de Hello o que seu viadinho de bosta!!!\n");
 		printf("Noooooooosssssaaaaaaaaaa!!!!! Como vc e groooossssssooooooooo!!!!!!! :P \n");
 		printf("Voce ainda nao me viu pelado!!! >( \n");
+		printf("Aiiii queeee tuuuuudooooo!!!!!!!! :P \n");
 		  return 0;
 }
