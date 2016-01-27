ed287669c20d9b092cfb7c722d2f47cbe11e467d
Willian Silva <willian.silva@concretesolutions.com.br>
2016-01-27 15:02:17 -0200

    Monitorando tudo!
    Monitorando tudo!!!

--- hello.c
+++ hello.c
@@ -6,5 +6,6 @@ int main()
 		printf("Noooooooosssssaaaaaaaaaa!!!!! Como vc e groooossssssooooooooo!!!!!!! :P \n");
 		printf("Voce ainda nao me viu pelado!!! >( \n");
 		printf("Aiiii queeee tuuuuudooooo!!!!!!!! :P \n");
+		printf("Para de viadeza!!! O Slack esta configurado com Jenkins, e esta monitarando tudo!! \n");
 		  return 0;
 }
