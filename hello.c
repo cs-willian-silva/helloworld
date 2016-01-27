676a2d10580c30b1c7bec84827cab319549ca5d0
Willian Silva <willian.silva@concretesolutions.com.br>
2016-01-27 14:00:58 -0200

    Groooosssssssoooooo!!!!!!!
    Aaaaaaiii paaaraaaaaaaaaaaa!!!!

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 int main()
 {
 	  printf("Hello world\n");
-		printf("Que merda de Hello o que seu viadinho de bosta!!!");
+		printf("Que merda de Hello o que seu viadinho de bosta!!!\n");
+		printf("Noooooooosssssaaaaaaaaaa!!!!! Como vc e groooossssssooooooooo!!!!!!! :P \n");
 		  return 0;
 }
