17c7bcc9cd55e1437277156649596217f18e2c52
Willian Silva <willian.silva@concretesolutions.com.br>
2016-01-27 14:10:33 -0200

    sou mesmo
    >[

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main()
 	  printf("Hello world\n");
 		printf("Que merda de Hello o que seu viadinho de bosta!!!\n");
 		printf("Noooooooosssssaaaaaaaaaa!!!!! Como vc e groooossssssooooooooo!!!!!!! :P \n");
+		printf("Voce ainda nao me viu pelado!!! >( \n");
 		  return 0;
 }
